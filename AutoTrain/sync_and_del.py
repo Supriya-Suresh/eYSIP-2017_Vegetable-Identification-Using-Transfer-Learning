96d5223284283ed928ca4d097b99f82fc6765974
TechieBoy <techieboy.teknas@gmail.com>
2017-07-06 09:59:16 +0530

    Added script for syncing and deleting remote files

---
+++ AutoTrain/sync_and_del.py
@@ -0,0 +1,24 @@
+import subprocess
+import sys
+SERVER_SSH = 'greenhouse@k-yantra.org'
+SERVER_DIR = '/var/www/Ghfarm/django/Autotrain/'
+LOCAL_DIR = '/home/weighing_scale/AutoTrain/Images/'
+
+sync_str = 'rsync -auvz ' + SERVER_SSH + ':' + SERVER_DIR + ' ' + LOCAL_DIR
+
+try:
+    process = subprocess.check_call(sync_str.split(), stdout=subprocess.PIPE)
+except Exception as e:
+    print(e)
+    sys.exit()
+output, error = process.communicate()
+print(output, error)
+
+rm_str = 'ssh ' + SERVER_SSH ' rm -r ' + SERVER_DIR
+
+try:
+    process = subprocess.check_call(sync_str.split(), stdout=subprocess.PIPE)
+except Exception as e:
+    print(e)
+output, error = process.communicate()
+print(output, error)
